955f54bb02d1f76c6db7446efee2342a34c6e8c8
Shota Jokhadze <jokhadzeshota8@gmail.com>
2024-09-29 14:41:04 +0400

    add about and assignment content

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,6 +2,11 @@ import './About.scss';
 
 export default function About() {
   return (
-    <div>About</div>
+    <>
+      <div className="about-container">
+        <h2>Project Info</h2>
+        <p>This is the project which is made along the homework for TBC React Academy</p>
+      </div>
+    </>
   )
 }
